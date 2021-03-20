ef42d791f3fe7276f0a30a6b85e2d204868ad102
Devin Spikowski <vegeta897@gmail.com>
2021-03-20 14:45:50 -0400

    :sparkles: Lines!

--- src/three-app.ts
+++ src/three-app.ts
@@ -1,9 +1,13 @@
 import {
 	BoxGeometry,
+	BufferGeometry,
+	Line,
+	LineBasicMaterial,
 	Mesh,
 	MeshBasicMaterial,
 	PerspectiveCamera,
 	Scene,
+	Vector3,
 	WebGLRenderer,
 } from 'three'
 
@@ -24,10 +28,22 @@ export class ThreeApp {
 		const material = new MeshBasicMaterial({ color: 0x00ff00 })
 		const cube = new Mesh(geometry, material)
 		this.scene.add(cube)
+
+		const lineMaterial = new LineBasicMaterial({ color: 0x0000ff })
+		const points = []
+		points.push(new Vector3(-1, 0, 0))
+		points.push(new Vector3(0, 1, 0))
+		points.push(new Vector3(1, 0, 0))
+		points.push(new Vector3(0, -1, 0))
+		points.push(new Vector3(-1, 0, 0))
+		const lineGeometry = new BufferGeometry().setFromPoints(points)
+		const line = new Line(lineGeometry, lineMaterial)
+		this.scene.add(line)
+
 		this.camera.position.z = 5
 	}
 	update() {
-		this.scene.children.forEach((child) => child.rotateX(0.01).rotateY(0.01))
+		this.scene.children.forEach((child) => child.rotateX(0.01).rotateY(0.005))
 	}
 	render() {
 		this.renderer.render(this.scene, this.camera)
