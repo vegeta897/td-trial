1c55308f0fdf6e839d7ab2f6270bec7d07d6b71e
Devin Spikowski <vegeta897@gmail.com>
2021-03-20 20:34:14 -0400

    :recycle: Simplify component class

--- src/components/com_object3d.ts
+++ src/components/com_object3d.ts
@@ -1,9 +1,5 @@
 import { Object3D } from 'three'
 
 export default class ThreeObject3D {
-	object3D: Object3D
-	foo: number
-	constructor(object3D: Object3D) {
-		this.object3D = object3D
-	}
+	constructor(public object3D: Object3D) {}
 }
