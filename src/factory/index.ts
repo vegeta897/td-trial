06ec3258afa3f6503b01507d755b6e1b815c5d2b
Devin Spikowski <vegeta897@gmail.com>
2021-04-02 03:30:02 -0400

    :recycle: Destructure createGameObject options

--- src/factory/index.ts
+++ src/factory/index.ts
@@ -10,36 +10,42 @@ import { createCube } from '../three/objects'
 import ThreeObject3D from '../components/com_object3d'
 import Transform3D from '../components/com_transform3d'
 
-interface IGameObject {
-	container?: Object3D
-	position?: Vector3
-	rotation?: Euler
-	scale?: Vector3
-	materialParams?: MeshLambertMaterialParameters
-	gameObjectType?: GameObjectTypes
-	additionalComponents?: Component[]
-	children?: Object3D[]
-}
-
 export default class Factory {
 	constructor(public game: Game) {}
 
-	createGameObject(options: IGameObject = {}) {
-		const cube = createCube(options.materialParams)
-		if (options.children) options.children.forEach((child) => cube.add(child))
-		;(options.container || this.game.threeApp.scene).add(cube)
-		const entity = this.game.world.create(
-			new ThreeObject3D(cube),
-			new Transform3D(options.position, options.rotation, options.scale)
-		)
-		cube.userData.entity = entity
-		if (options.additionalComponents)
-			this.game.world.insert(entity, ...options.additionalComponents)
-		if (options.gameObjectType)
-			this.game.world.emplace(entity, Tag.for(options.gameObjectType))
-	}
 	createSpawner = createSpawner
 	createEnemy = createEnemy
 	createTurret = createTurret
 	createBullet = createBullet
+	createGameObject({
+		container,
+		position,
+		rotation,
+		scale,
+		materialParams,
+		gameObjectType,
+		additionalComponents,
+		children,
+	}: {
+		container?: Object3D
+		position?: Vector3
+		rotation?: Euler
+		scale?: Vector3
+		materialParams?: MeshLambertMaterialParameters
+		gameObjectType?: GameObjectTypes
+		additionalComponents?: Component[]
+		children?: Object3D[]
+	} = {}) {
+		const cube = createCube(materialParams)
+		if (children) children.forEach((child) => cube.add(child))
+		;(container || this.game.threeApp.scene).add(cube)
+		const entity = this.game.world.create(
+			new ThreeObject3D(cube),
+			new Transform3D(position, rotation, scale)
+		)
+		cube.userData.entity = entity
+		if (additionalComponents)
+			this.game.world.insert(entity, ...additionalComponents)
+		if (gameObjectType) this.game.world.emplace(entity, Tag.for(gameObjectType))
+	}
 }
