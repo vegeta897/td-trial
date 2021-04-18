ab39ef06920a09761e3a27a1195a900b68f9eb2d
Devin Spikowski <vegeta897@gmail.com>
2021-04-18 01:53:54 -0400

    :recycle: Parallelize asset loading

--- src/three/assets.ts
+++ src/three/assets.ts
@@ -5,26 +5,27 @@ import { Mesh } from 'three'
 import turretObj from '../assets/turret.obj'
 import loaderObj from '../assets/loader.obj'
 
-export async function loadAssets(this: ThreeApp) {
-	const loader = new OBJLoader()
-	const turretGroup = await loader.loadAsync(turretObj)
-	const turretGeometry = BufferGeometryUtils.mergeVertices(
-		(<Mesh>turretGroup.children[0]).geometry,
-		0.01
-	)
-	turretGeometry.translate(0, -0.5, 0)
-	this.assets.set(AssetNames.TurretGeometry, turretGeometry)
-
-	const loaderGroup = await loader.loadAsync(loaderObj)
-	const loaderGeometry = BufferGeometryUtils.mergeVertices(
-		(<Mesh>loaderGroup.children[0]).geometry,
-		0.01
-	)
-	loaderGeometry.translate(0, -0.5, 0)
-	this.assets.set(AssetNames.LoaderGeometry, loaderGeometry)
-}
-
 export enum AssetNames {
 	TurretGeometry,
 	LoaderGeometry,
+}
+
+const assetList: [AssetNames, string][] = [
+	[AssetNames.TurretGeometry, turretObj],
+	[AssetNames.LoaderGeometry, loaderObj],
+]
+
+export async function loadAssets(this: ThreeApp) {
+	const loader = new OBJLoader()
+	await Promise.all(
+		assetList.map(async ([assetName, objPath]) => {
+			const group = await loader.loadAsync(objPath)
+			const geometry = BufferGeometryUtils.mergeVertices(
+				(<Mesh>group.children[0]).geometry,
+				0.01
+			)
+			geometry.translate(0, -0.5, 0)
+			this.assets.set(assetName, geometry)
+		})
+	)
 }
