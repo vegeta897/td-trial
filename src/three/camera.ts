938339b9982eed8270e2cff5d27e2559f85873ce
Devin Spikowski <vegeta897@gmail.com>
2021-04-20 00:06:01 -0400

    :recycle: Destructure ThreeApp

--- src/three/camera.ts
+++ src/three/camera.ts
@@ -2,34 +2,30 @@ import CameraControls from 'camera-controls'
 import { ThreeApp } from './three-app'
 import { OrthographicCamera } from 'three'
 
-export function setupCamera(threeApp: ThreeApp) {
+export function setupCamera({ camera, cameraControls, center }: ThreeApp) {
 	// Set up camera
-	threeApp.camera.position.setFromSphericalCoords(
+	camera.position.setFromSphericalCoords(
 		100, // Distance
 		Math.PI / 4, // Pitch
 		(Math.PI * 2) / 6 // Yaw
 	)
-	threeApp.camera.lookAt(threeApp.center)
+	camera.lookAt(center)
 
 	// Set up camera controls
-	threeApp.cameraControls.distance = 100
-	threeApp.cameraControls.minZoom = 0.1
-	threeApp.cameraControls.maxZoom = 7
-	threeApp.cameraControls.maxPolarAngle = Math.PI * 0.45
-	threeApp.cameraControls.setTarget(
-		threeApp.center.x,
-		threeApp.center.y,
-		threeApp.center.z
-	)
-	threeApp.cameraControls.polarAngle = Math.PI / 4
-	threeApp.cameraControls.azimuthAngle = (Math.PI * 2) / 6
-	threeApp.cameraControls.mouseButtons.left = CameraControls.ACTION.NONE
-	threeApp.cameraControls.mouseButtons.right = CameraControls.ACTION.ROTATE
-	threeApp.cameraControls.mouseButtons.middle = CameraControls.ACTION.TRUCK
-	threeApp.cameraControls.polarRotateSpeed = 0.5
-	threeApp.cameraControls.azimuthRotateSpeed = 0.7
-	threeApp.cameraControls.dampingFactor = 0.01
-	threeApp.cameraControls.draggingDampingFactor = 0.02
+	cameraControls.distance = 100
+	cameraControls.minZoom = 0.1
+	cameraControls.maxZoom = 7
+	cameraControls.maxPolarAngle = Math.PI * 0.45
+	cameraControls.setTarget(center.x, center.y, center.z)
+	cameraControls.polarAngle = Math.PI / 4
+	cameraControls.azimuthAngle = (Math.PI * 2) / 6
+	cameraControls.mouseButtons.left = CameraControls.ACTION.NONE
+	cameraControls.mouseButtons.right = CameraControls.ACTION.ROTATE
+	cameraControls.mouseButtons.middle = CameraControls.ACTION.TRUCK
+	cameraControls.polarRotateSpeed = 0.5
+	cameraControls.azimuthRotateSpeed = 0.7
+	cameraControls.dampingFactor = 0.01
+	cameraControls.draggingDampingFactor = 0.02
 }
 
 export function updateCamera(camera: OrthographicCamera, scale: number) {
