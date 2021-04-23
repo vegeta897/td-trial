1e63df1ac51431cd84eccd40687ba38866900a1a
Devin Spikowski <vegeta897@gmail.com>
2021-04-23 02:41:45 -0400

    :bug: Use previous transform to avoid doubled velocity

--- src/systems/sys_physics.ts
+++ src/systems/sys_physics.ts
@@ -10,21 +10,28 @@ import Velocity3D from '../components/com_velocity3d'
 export default class PhysicsSystem extends System {
 	bodies = this.world.view(PhysicsBody, Transform3D)
 	update(tick: number) {
-		this.bodies.each((entity, { body }, transform) => {
-			if (body.type === CANNON.Body.DYNAMIC) return
-			// For static and kinetic bodies, ECS is the source of truth
-			if (transform.lastUpdated === tick) {
-				body.position.copy(<CANNON.Vec3>(<unknown>transform.position))
-				body.quaternion.copy(<CANNON.Quaternion>(<unknown>transform.rotation))
+		this.bodies.each(
+			(entity, { body }, { lastUpdated, prevPosition, prevRotation }) => {
+				if (body.type === CANNON.Body.DYNAMIC) return
+				// For static and kinetic bodies, ECS is the source of truth
+				if (lastUpdated === tick) {
+					// Previous transform is used because velocity/rotation will be applied
+					body.position.copy(<CANNON.Vec3>(<unknown>prevPosition))
+					body.quaternion.copy(<CANNON.Quaternion>(<unknown>prevRotation))
+				}
+				if (body.type === CANNON.Body.STATIC) return
+				const velocity = this.world.get(entity, Velocity3D)
+				if (velocity)
+					body.velocity.copy(
+						<CANNON.Vec3>(
+							(<unknown>(
+								velocity.vector3.clone().multiplyScalar(this.game.tickRate)
+							))
+						)
+					)
+				else body.velocity.setZero()
 			}
-			if (body.type === CANNON.Body.STATIC) return
-			const velocity = this.world.get(entity, Velocity3D)
-			if (velocity)
-				body.velocity.copy(
-					<CANNON.Vec3>(<unknown>velocity.vector3.clone().multiplyScalar(60))
-				)
-			else body.velocity.setZero()
-		})
+		)
 		this.game.physics.world.step(1 / this.game.tickRate)
 		this.bodies.each((entity, { body }, transform) => {
 			if (body.type !== CANNON.Body.DYNAMIC) return
