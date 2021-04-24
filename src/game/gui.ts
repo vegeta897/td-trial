de1f4b4f07c49f37322ad57be2a11fcf6dd6c193
Devin Spikowski <vegeta897@gmail.com>
2021-04-24 17:16:58 -0400

    :lipstick: Add stats panel

--- src/game/gui.ts
+++ src/game/gui.ts
@@ -3,7 +3,8 @@ import { InteractionState } from './interaction'
 
 export default class GUI {
 	game: Game
-	container = document.createElement('div')
+	statsContainer = document.createElement('div')
+	buildContainer = document.createElement('div')
 	buttons = {
 		turret: document.createElement('button'),
 		loader: document.createElement('button'),
@@ -11,32 +12,47 @@ export default class GUI {
 	}
 	constructor(game: Game) {
 		this.game = game
-		this.container.id = 'gui-container'
-		document.body.appendChild(this.container)
+
+		this.statsContainer.id = 'gui-stats-container'
+		document.body.appendChild(this.statsContainer)
+		const tickDisplay = document.createElement('span')
+		const entityDisplay = document.createElement('span')
+		const bodyDisplay = document.createElement('span')
+		this.statsContainer.appendChild(tickDisplay)
+		this.statsContainer.appendChild(entityDisplay)
+		this.statsContainer.appendChild(bodyDisplay)
+		setInterval(() => {
+			tickDisplay.innerText = `Tick: ${this.game.tick}`
+			entityDisplay.innerText = `Entities: ${this.game.world.size()}`
+			bodyDisplay.innerText = `Bodies: ${this.game.physics.world.bodies.length}`
+		}, 200)
+
+		this.buildContainer.id = 'gui-build-container'
+		document.body.appendChild(this.buildContainer)
 		const heading = document.createElement('h2')
 		heading.innerText = 'Build'
-		this.container.appendChild(heading)
+		this.buildContainer.appendChild(heading)
 
 		this.buttons.turret.innerText = 'Turret'
 		this.buttons.turret.addEventListener('click', () => {
 			this.game.interaction.state = InteractionState.BUILD_TURRET
 			this.update()
 		})
-		this.container.appendChild(this.buttons.turret)
+		this.buildContainer.appendChild(this.buttons.turret)
 
 		this.buttons.loader.innerText = 'Loader'
 		this.buttons.loader.addEventListener('click', () => {
 			this.game.interaction.state = InteractionState.BUILD_LOADER
 			this.update()
 		})
-		this.container.appendChild(this.buttons.loader)
+		this.buildContainer.appendChild(this.buttons.loader)
 
 		this.buttons.cancel.innerText = 'cancel'
 		this.buttons.cancel.addEventListener('click', () => {
 			this.game.interaction.state = InteractionState.NONE
 			this.update()
 		})
-		this.container.appendChild(this.buttons.cancel)
+		this.buildContainer.appendChild(this.buttons.cancel)
 		this.update()
 	}
 	update() {
