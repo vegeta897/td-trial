9255fe5df0062d14bf1f6c565c20415a376aeabe
Devin Spikowski <vegeta897@gmail.com>
2021-04-28 01:51:50 -0400

    :zap: Use SplitSolver

--- src/game/physics.ts
+++ src/game/physics.ts
@@ -1,17 +1,27 @@
-import { Vec3, World, Material, ContactMaterial, GSSolver } from 'cannon-es'
+import {
+	Vec3,
+	World,
+	Material,
+	ContactMaterial,
+	SplitSolver,
+	GSSolver,
+} from 'cannon-es'
 
 export class Physics {
+	solver = new SplitSolver(new GSSolver())
 	world = new World({
 		gravity: new Vec3(0, -20, 0),
 		allowSleep: true,
 		quatNormalizeFast: true, // Turn this off if things are unstable
+		solver: this.solver,
 	})
 	static Materials = {
 		ground: new Material('ground'),
 		tumbler: new Material('tumbler'),
 	}
 	constructor() {
-		;(<GSSolver>this.world.solver).iterations = 10
+		this.solver.iterations = 10
+		this.solver.tolerance = 1e-7
 		this.world.addContactMaterial(
 			new ContactMaterial(Physics.Materials.ground, Physics.Materials.tumbler, {
 				friction: 100,
